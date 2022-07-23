e2bfc5b62325ad680540b84671aca5412fa01613
Tyler M <tmorse2222@gmail.com>
2022-07-23 07:35:15 -0700

    Linked search entry to OpenWeather Geocoding API to return lat and long for weather retrieval

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,15 @@ var searchBtn = $(`#searchBtn`);
 
 
 function search() {
-    var searchText = document.getElementById(`searchText`).value;
-    console.log(searchText);
+    var city = document.getElementById(`searchText`).value;
+    var geocode = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=ad2ab0eea4f63232dfe1a7f79251420d`
+    fetch(geocode)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  })
 };
 
 searchBtn.click(search);

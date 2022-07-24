1fe8f7ad815da3e9a9398a212b77fc7005d88547
Tyler M <tmorse2222@gmail.com>
2022-07-23 17:11:48 -0700

    Linked Geolocation with current weather API

--- assets/script.js
+++ assets/script.js
@@ -11,8 +11,14 @@ function search() {
   .then(function (data) {
     var lat = data[0].lat;
     var lon = data[0].lon;
+    var currentWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ad2ab0eea4f63232dfe1a7f79251420d`
+   fetch(currentWeather)
+   .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
     console.log(data);
-    console.log(`${lat} + ${lon}`);
+  })
   })
 };
 

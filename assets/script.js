feb80eabdc91596a18bddbd820f6adb02816bd55
Tyler M <tmorse2222@gmail.com>
2022-07-23 23:20:30 -0700

    added to function to display date of first of five day forcast

--- assets/script.js
+++ assets/script.js
@@ -3,20 +3,26 @@ var searchBtn = $(`#searchBtn`);
 
 function search() {
     var city = document.getElementById(`searchText`).value;
-    var geocode = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=ad2ab0eea4f63232dfe1a7f79251420d`
+    var geocode = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=ad2ab0eea4f63232dfe1a7f79251420d`;
+
     fetch(geocode)
+
   .then(function (response) {
     return response.json();
   })
+
   .then(function (data) {
     var lat = data[0].lat;
     var lon = data[0].lon;
-    var currentWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ad2ab0eea4f63232dfe1a7f79251420d&units=imperial`
+    var currentWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ad2ab0eea4f63232dfe1a7f79251420d&units=imperial`;
+    var fiveDay = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=ad2ab0eea4f63232dfe1a7f79251420d&units=imperial`;
 
    fetch(currentWeather)
+
    .then(function (response) {
     return response.json();
   })
+
   .then(function (data) {
     var currentCity = data.name;
     var timestamp = data.dt;
@@ -44,11 +50,23 @@ function search() {
    url: `https://api.openuv.io/api/v1/uv?lat=${lat}&lng=${lon}`,
    success: function(response) {
     var uvIndex = response.result.uv;
-    $(`#uvIndex`).text(`Index: ${uvIndex}`);
+    $(`#uvIndex`).text(`UV Index: ${uvIndex}`);
    },
+  });
+
+  fetch(fiveDay)
+
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    var futureTimestamp = data.list[0].dt;
+    var futureDate = new Date(futureTimestamp*1000).toLocaleDateString();
+    $(`#dayOneDate`).text(futureDate);
+    console.log(data);
   })
 
-
-})}
+})
+}
 
 searchBtn.click(search);
